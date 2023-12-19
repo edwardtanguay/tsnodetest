e741415c8d4165db282de57d8f7a8010daa0db82
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-19 20:31:12 +0100

    only one tsconfig

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as config from '../config.js';
 const app = express();
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.status(200).json({ message: 'worked' });
 });
 app.listen(config.port(), () => {
